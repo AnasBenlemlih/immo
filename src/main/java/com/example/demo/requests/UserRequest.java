f10d703c6b46e7fd7cffa59cff052cea92da5933
AnasBenlemlih <anas.benlemlih@gmail.com>
2020-10-09 00:41:49 +0100

    Expressions requlieres password check input

--- src/main/java/com/example/demo/requests/UserRequest.java
+++ src/main/java/com/example/demo/requests/UserRequest.java
@@ -1,12 +1,14 @@
 package com.example.demo.requests;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class UserRequest {
 	
-	@NotNull(message = "Ce champ ne doit etre null !! ")
+	@NotBlank(message = "Ce champ ne doit etre null !! ")
 	@Size(min=3,message = "Min 3 caracteres")
 	private String firstName;
 
@@ -20,7 +22,8 @@ public class UserRequest {
 	private String email;
 	
 	@NotNull(message = "Ce champ ne doit etre null !! ")
-	@Size(min=6,message = "Min 3 caracteres et max 15 ")
+	@Pattern(regexp = "(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$",
+			message = "Password (UpperCase, LowerCase, Number/SpecialChar and min 8 Chars)")
 	private String password;
 	
 
