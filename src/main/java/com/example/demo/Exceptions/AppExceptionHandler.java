1197ce449eadf67a84e850023b69ba5f50f52066
AnasBenlemlih <anas.benlemlih@gmail.com>
2020-10-09 00:33:24 +0100

    Manipuler l'exception du validation

--- src/main/java/com/example/demo/Exceptions/AppExceptionHandler.java
+++ src/main/java/com/example/demo/Exceptions/AppExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.example.demo.Exceptions;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -26,5 +29,14 @@ public class AppExceptionHandler {
 		
 		ErrorMessage errorMessage = new  ErrorMessage(new Date(), ex.getMessage());
 		return new ResponseEntity<>(errorMessage,new HttpHeaders(),HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(value = MethodArgumentNotValidException.class)
+	public ResponseEntity<Object> HandleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request){
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getFieldErrors().forEach(error ->
+		errors.put(error.getField(), error.getDefaultMessage()));
+		
+		return new ResponseEntity<>(errors,new HttpHeaders(),HttpStatus.BAD_REQUEST);
 	}
 }
